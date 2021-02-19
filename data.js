0f769aa9c1ff5d558b1a6580122a4930bdac5bbd
nnafzaoui <nafzaouinourine@gmail.com>
2021-02-19 17:10:22 +0100

    some progress

---
+++ data.js
@@ -0,0 +1,56 @@
+var vehicls = {
+    moto : {
+        id: 1,
+        name : "Moto",
+        img : "img/moto.png",
+        engine : {
+            type1 : "Electrique",
+            type2 : "Essence", 
+        },
+        bv : false,
+        tarif : 10
+    },
+    citadine : {
+        id : 2,
+        name :  "Citadine",
+        img : "img/citadine.png",
+        engine : {
+            type1 : "Electrique",
+            type2 : "Hybride",
+            type3 : "Essance",
+            type4 : "Diesel"
+        },
+        bv : "Manuelle",
+        tarif : 12,
+    },
+    compact : {
+        id : 3,
+        name : "Compact",
+        img : "img/compact.png",
+        engine : {
+            type1 : "Hybride",
+            type2 : "Essance",
+            type3 : "Diesel"
+        },
+        bv : "Manuelle",
+        tarif : 14,
+    },
+    berline : {
+        id : 4,
+        name : "Berline",
+        img : "img/berline.png",
+        engine : {
+            type1 : "Hybride",
+            type2 : "Essance",
+            type3 : "Diesel"
+        },
+        bv : "Automatique",
+        tarif : 20,
+    },
+    utilitaire : {
+        id : 5,
+        name : "Utilitaire",
+        img : "img/"
+    }
+
+}
