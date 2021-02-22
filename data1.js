85213943919ef299cf00bb3aea5a34625fcd637e
nnafzaoui <nafzaouinourine@gmail.com>
2021-02-22 09:51:48 +0100

    gallery optimization

--- data1.js
+++ data1.js
@@ -1,44 +1,37 @@
 var type = {
     "Moto":{
         "carburant":["Essence","Electrique"],
-        "boite":"",
+        "boite":"none",
         "prix":10,
-        img:"img/moto.png",
     },
     "Citadine" : {
         "carburant" : ["Electrique","Hybride", "Essance", "Diesel"],
         "boit":"Manuelle",
         "prix": 12,
-        img : "img/citadine.png",
     },
     "Compact":{
         "carburant":["Hybride", "Essance", "Diesel"],
         "boit": "Manuelle",
         "prix": 14,
-        img : "img/compact.png"
     },
     "Berline":{
         "carburant":["Hybride", "Essance", "Diesel"],
         "boit": "Automatique",
         "prix":20,
-        img: "img/berline.png"
     },
     "Utilitaire": {
-        "carburant":"Diesel",
+        "carburant":["Diesel"],
         "boit": "Manuelle",
         "prix": 16,
-        img: "img/util.png"
     },
     "Engin de Chantier" : {
         "carburant":["Essance","Diesel"],
         "boit":"Manuelle",
         "prix":900,
-        img: "img/trax.png"
     },
     "Camion": {
-        "carburant": "Diesel",
+        "carburant": ["Diesel"],
         "boit": "Automatique",
         "prix": 250,
-        img : "img/camion.png"
     }
 }
---
+++ script/hamMenu.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var menuDropdown = document.getElementById('mobileMenu');
+    if (menuDropdown.style.display === "none") {
+      menuDropdown.style.display = "block";
+    } else {
+      menuDropdown.style.display = "none";
+    }
+  }
